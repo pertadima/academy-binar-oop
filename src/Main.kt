ff566b045f6c6ee7765edfe464db33de3c7eceaa
Irfan Pertadima <pertadimairfan@gmail.com>
2019-04-08 16:08:58 +0700

    [irfan] object calling

--- src/Main.kt
+++ src/Main.kt
@@ -4,5 +4,9 @@ fun main() {
     val manusia1 = Manusia("Irfan", "Bogor", "19 Mei 1995" , "Jogja" )
     val manusia2 = Manusia("Donny", "Sleman", "20 Mei 1993" , "Jogja" )
 
+    manusia1.bernafas("Paru-paru")
+    manusia1.berjalan("Kantor")
 
+    manusia2.tampilIdentitas()
+    manusia2.berjalan()
 }
